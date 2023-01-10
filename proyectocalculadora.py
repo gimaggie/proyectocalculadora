8ae6722b24fd9f2e91f8b5588fd8c1d74234f526
MARGARITA GIRON CHANEZ <949245@horusur.corp>
2023-01-10 11:43:57 -0600

    Se agrega funcion para calcular el exponente

--- proyectocalculadora.py
+++ proyectocalculadora.py
@@ -1,4 +1,7 @@
+###Proyecto calculadora
+###Margarita Giron Chanez
 from math import sqrt
+import math
 def S(a, b):
     return a + b
 def R(a, b):
@@ -10,22 +13,51 @@ def D(a, b):
 def Z(a):
     return sqrt(n1)
 while True:
-    print("Menú")
+    print("***Menú***")
     print("1. Sumar")
     print("2. Restar")
     print("3. Multiplicar")
     print("4. Dividir")
     print("5. Raíz")
+    print("6. Exponente")
+    print("7. Salir")
     opc = input("Ingresa una opción: ")
-    n1 = float(input("Ingresa el número 1: "))
-    n2 = float(input("Ingresa el número 2: "))
     if opc == "1":
+        n1 = float(input("Ingresa el número 1: "))
+        n2 = float(input("Ingresa el número 2: "))
         print("La suma es: ", S(n1, n2))
     elif opc == "2":
-        print("La resta es:", R(n1, n2))
+        n1 = float(input("Ingresa el número 1: "))
+        n2 = float(input("Ingresa el número 2: "))
+        print("La resta es: ", R(n1, n2))
     elif opc == "3":
-        print("La división es:", M(n1, n2))
+        n1 = float(input("Ingresa el número 1: "))
+        n2 = float(input("Ingresa el número 2: "))
+        print("La división es: ", M(n1, n2))
     elif opc == "4":
-        print("La multiplicación es", D(n1, n2))
-    elif opc =="5":
-        print("La raíz del primer número es:", Z(n1))
+        n1 = float(input("Ingresa el número 1: "))
+        n2 = float(input("Ingresa el número 2: "))
+        print("La multiplicación es: ", D(n1, n2))
+    elif opc == "5":
+        num = float(input("Ingresa el número para obtener la raíz: "))
+        raiz = sqrt(num)
+        print("La raíz es: ", raiz)
+    elif opc == "6":
+        base = int(input("Ingresa la base: "))
+        exponente = int(input("Ingresa el exponente: "))
+        resultado = 1
+        i = 0
+        while True:
+               if exponente == 0:
+                 print("El resultado es 1: ")
+                 break
+               resultado = base * resultado
+               i = i + 1
+               if i >= abs(exponente):
+                 if exponente < 0:
+                   resultado = 1 / resultado
+                 print("El resultado es: ", resultado)
+                 break
+    elif opc == "7":
+        print("¡Hasta luego!")
+        break
